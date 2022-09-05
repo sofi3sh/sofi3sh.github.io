7be59a2a9b43761cd535703b122c172e4dc8936e
sofi <sofi3sh@gmail.com>
2022-09-05 09:44:05 +0300

    first commit

---
+++ assets/js/subject.js
@@ -0,0 +1,97 @@
+function Logo(){
+
+  return (
+    <>
+    <div className="logo logo-width-1">
+                        <a href="index.html"><img src="assets/imgs/theme/logo.svg" alt="logo"/></a>
+                    </div>
+    </>
+  );
+
+}
+ReactDOM.render(<Logo />, document.getElementById('logo'));
+    
+        function MyApp() {
+          return (
+            <>
+ <section className="featured section-padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay="0">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-1.svg" alt=""/>
+                            </div>
+                            <div className="banner-text">
+                                <h3 class="icon-box-title">Best prices & offers</h3>
+                                <p>Orders $50 or more</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-2.svg" alt=""/>
+                            </div>
+                            <div className="banner-text">
+                                <h3 className="icon-box-title">Free delivery</h3>
+                                <p>24/7 amazing services</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-3.svg" alt=""/>
+                            </div>
+                            <div className="banner-text">
+                                <h3 className="icon-box-title">Great daily deal</h3>
+                                <p>When you sign up</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-4.svg" alt=""/>
+                            </div>
+                            <div className="banner-text">
+                                <h3 className="icon-box-title">Wide assortment</h3>
+                                <p>Mega Discounts</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-5.svg" alt=""/>
+                            </div>
+                            <div claclassNamess="banner-text">
+                                <h3 className="icon-box-title">Easy returns</h3>
+                                <p>Within 30 days</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
+                        <div className="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
+                            <div className="banner-icon">
+                                <img src="assets/imgs/theme/icons/icon-6.svg" alt=""/>
+                            </div>
+                            <div className="banner-text">
+                                <h3 className="icon-box-title">Safe delivery</h3>
+                                <p>Within 30 days</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+          );
+        }
+
+  
+   ReactDOM.render(<MyApp />, document.getElementById('test'));
+   ReactDOM.render(<MyApp />, document.getElementById('demo'));
+
+
